a3c9c43cf7ecc597765abcc93fa3a84fa541fe58
mctmarcus3 <mctmarcus3@gmail.com>
2025-10-21 14:42:14 +0800

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # The quotes and the () at the end are crucial.
-gunicorn --workers 4 --bind 0.0.0.0:8000 'app:create_app()' --access-logfile ./log/gunicorn-access.log --error-logfile ./log/gunicorn-error.log --capture-output ./log/gunicorn-output.log --log-level debug 
+gunicorn --workers 4 --bind 0.0.0.0:8000 'app:create_app()' --access-logfile ./log/gunicorn-access.log --error-logfile ./log/gunicorn-error.log --capture-output --log-level debug 
