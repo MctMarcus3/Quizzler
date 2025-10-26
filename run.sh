20208822923785505dcda90c49aa32195f18bd34
mctmarcus3 <mctmarcus3@gmail.com>
2025-10-26 17:54:26 +0800

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # The quotes and the () at the end are crucial.
-source venv/bin/activate
+source /venv/bin/activate
 gunicorn --workers 4 --bind 0.0.0.0:8000 'app:create_app()' --access-logfile ./logs/gunicorn-access.log --error-logfile ./logs/gunicorn-error.log --capture-output --log-level debug --timeout 120
